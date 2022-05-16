1d5cc046132bb7d27988c7a661d017c5ef0db45f
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-05-16 12:18:14 -0700

    Update Celebrity.java

--- Celebrity.java
+++ Celebrity.java
@@ -1,3 +1,31 @@
-public class Celebrity {
-    
+public class Celebrity{
+
+private String name; 
+
+private String clue; 
+private String type;
+
+public Celebrity(String name,String clue, String type){
+this.name = name; 
+this. clue = clue; 
+this.type = type;
+}
+
+public String getName(){
+    return name; 
+}
+
+public String getClue(){
+    return clue; 
+}
+
+public String getType(){
+return type;
+}
+public void checkClue(){
+    System.out.println("Your clue is valid");
+
+}
+
+
 }
